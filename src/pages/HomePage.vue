0b2991393563d0bafc87606abdabf2ccc8c1cfb4
Jonesy <jonesyjava@gmail.com>
2021-05-08 17:28:00 -0600

    Fluid

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column">
+  <div class="home flex-grow-1 d-flex flex-column container-fluid">
     <div>
       <div class="row justify-content-center mt-5">
         <h1 class="wthr-font">CURRENT</h1>
